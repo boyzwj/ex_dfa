a7a3f28d2a47fbdc05f6369c59878fcfdfd4088a
Tom@ZhangWenjie <boyzwj@qq.com>
2022-10-28 18:22:34 +0800

    first

---
+++ lib/ex_dfa.ex
@@ -0,0 +1,18 @@
+defmodule ExDfa do
+  @moduledoc """
+  Documentation for `ExDfa`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExDfa.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_dfa_test.exs
@@ -0,0 +1,8 @@
+defmodule ExDfaTest do
+  use ExUnit.Case
+  doctest ExDfa
+
+  test "greets the world" do
+    assert ExDfa.hello() == :world
+  end
+end
